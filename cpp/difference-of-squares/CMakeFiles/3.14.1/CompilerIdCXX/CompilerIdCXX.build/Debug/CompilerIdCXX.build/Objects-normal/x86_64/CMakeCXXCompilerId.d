b085baa570cb55f65f475ebb75cfd06e6adce60d
PUNEET UGRU <puneetugru@gmail.com>
2019-04-11 22:48:12 +0530

    Exercism Exercise

---
+++ cpp/difference-of-squares/CMakeFiles/3.14.1/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/puugru/Exercism/cpp/difference-of-squares/CMakeFiles/3.14.1/CompilerIdCXX/CMakeCXXCompilerId.cpp
---
+++ cpp/difference-of-squares/difference_of_squares.cpp
@@ -0,0 +1,34 @@
+#include "difference_of_squares.h"
+#include <algorithm>
+
+using namespace std;
+
+namespace squares
+{
+
+int square_of_sum(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        sum += i;
+    }
+    return sum * sum;
+}
+
+int sum_of_squares(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        sum += i * i;
+    }
+    return sum;
+}
+
+int difference(int n)
+{
+    return square_of_sum(n) - sum_of_squares(n);
+}
+
+} // namespace squares
